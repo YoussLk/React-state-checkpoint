73257056c4d71c5ca64bd80258c4c2ede50460b5
YoussLk <122651677+YoussLk@users.noreply.github.com>
2023-03-30 17:43:31 +0200

    state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "LAKRIM Youssef",
+      bio: "Hi every one I am fullstack Developer ",
+      imgSrc: "https://via.placeholder.com/150",
+      profession: "Web Developer",
+    },
+    shows: false,
+    time: 0,
+    intervalId: null,
+  };
+
+  componentDidMount() {
+    const intervalId = setInterval(this.timer, 1000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  timer = () => {
+    this.setState((prevState) => ({
+      time: prevState.time + 1,
+    }));
+  };
+
+  toggleShow = () => {
+    if (this.state.shows) {
+      clearInterval(this.state.intervalId);
+      this.setState({ intervalId: null });
+    } else {
+      const intervalId = setInterval(this.timer, 1000);
+      this.setState({ intervalId });
+    }
+    this.setState((prevState) => ({
+      shows: !prevState.shows,
+      time: 0,
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { shows, time } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        {shows && (
+          <div>
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} alt="Profile" />
+            <p>{profession}</p>
+          </div>
+        )}
+        {shows && <p>Time since last component mounted: {time} seconds</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
